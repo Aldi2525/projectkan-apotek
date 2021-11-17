8794fe43dff231376546ff0d04cfe76d45a092f5
Aldi2525 <tzzxuan1933@gmail.com>
2021-11-17 11:30:53 +0700

    project apotek model

---
+++ database/migrations/2021_11_15_175636_create_barang_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->id();
+            $table->integer('kode_barang');
+            $table->string('nama_barang');
+            $table->integer('jumlah');
+            $table->integer('harga_beli');
+            $table->integer('harga_jual');
+            $table->bigInteger('kategori_id')->unsigned();
+            $table->string('gambar');
+            $table->string('keterangan');
+
+            $table->foreign("kategori_id")->references('id')
+            ->on('kategori')->onUpdate('cascade')
+            ->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barang');
+    }
+}
---
+++ database/migrations/2021_11_15_201649_create_b_keluar_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBKeluarTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('b_keluar', function (Blueprint $table) {
+            $table->id();
+            $table->integer('jumlah');
+            $table->integer('kode_bm');
+            $table->BigInteger('barang_id')->unsigned();
+            $table->integer('kode_trans');
+            $table->integer('jmlh');
+            $table->integer('harga_total');
+            $table->string('user');
+            $table->string('supplier');
+            $table->string('diskon');
+
+            $table->foreign("barang_id")->references('id')
+                ->on('barang')->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('b_keluar');
+    }
+}
